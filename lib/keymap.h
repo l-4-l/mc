1f224b3d6f3b72be97f4c1507421cdbea0cb290a
Slava Zanko <slavazanko@gmail.com>
2014-04-22 17:46:20 +0300

    Added new keymap implementation
    Signed-off-by: Slava Zanko <slavazanko@gmail.com>

---
+++ lib/keymap.h
@@ -0,0 +1,42 @@
+#ifndef MC__KEYMAP_H
+#define MC__KEYMAP_H
+
+/*** typedefs(not structures) and defined constants **********************************************/
+
+typedef struct
+{
+    const char *group;
+    const char *name;
+    const char *event_group;
+    const char *event_name;
+} mc_keymap_event_init_t;
+
+/*** enums ***************************************************************************************/
+
+/*** structures declarations (and typedefs of structures)*****************************************/
+
+/*** global variables defined in .c file *********************************************************/
+
+/*** declarations of public functions ************************************************************/
+
+gboolean mc_keymap_init (GError ** error);
+gboolean mc_keymap_deinit (GError ** error);
+
+gboolean mc_keymap_bind_keycode (const char *group, const char *name, char *pressed_keynames,
+                                 GError ** error);
+gboolean mc_keymap_bind_event (const char *group, const char *name, const char *event_group,
+                               const char *event_name, GError ** error);
+gboolean mc_keymap_mass_bind_event (mc_keymap_event_init_t *, GError ** error);
+
+gboolean mc_keymap_bind_switch_event (const char *group, const char *name, const char *switch_group,
+                                      GError ** error);
+
+gboolean mc_keymap_process_group (const char *group, long pressed_keycode, void *data,
+                                  event_return_t * ret, GError ** error);
+const char *mc_keymap_get_key_name_by_code (const char *group, long pressed_keycode,
+                                            GError ** error);
+
+
+/*** inline functions ****************************************************************************/
+
+#endif /* MC__KEYMAP_H */
